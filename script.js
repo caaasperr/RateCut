523673b760b2d5a990f1c7a3a8a7fd9bb690b776
Caaasperr <122859026+caaasperr@users.noreply.github.com>
2024-01-02 12:12:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function calculate() {
   var inputElement = document.getElementById("count");
-  var inputValue = parseInt(inputElement.value); // Parse the input value as an integer
+  var inputValue = parseInt(inputElement.value);
   var result = calRateCut(inputValue);
   let target = document.getElementById("container");
 
-  // Clear the container before appending new elements
   target.innerHTML = "";
 
   for (var i = 0; i < result.length; i++) {
     let targetTag = document.createElement('p');
     targetTag.innerHTML = Math.round(result[i]);
-    target.appendChild(targetTag); // Append to the container, not the input element
+    target.appendChild(targetTag);
   }
 }
 
@@ -24,7 +23,5 @@ function calRateCut(cc) {
   var r7 = cc * 0.89;
   var r8 = cc * 0.96;
   var r9 = cc * 1;
-
-  // Return an array containing the calculated values
   return [r1, r2, r3, r4, r5, r6, r7, r8, r9];
 }
